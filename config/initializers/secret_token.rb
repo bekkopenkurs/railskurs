d7c0035930f59c1993d60fc602700b4c6e46d49a
BEKK <bekk@ubuntu.(none)>
2011-04-03 11:24:59 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railskurs::Application.config.secret_token = '10c9032fc3cb949c3beea8fb27b3ae028c35fa30ececd9dbef7a5174a84d4f1b1355213b8d002c14bd99f465a0fa5b839be9ecbbe617df586f8dac52b7cf4f27'
